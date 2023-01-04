4190645a7c94db951b98fb58b4171a2398149b8a
naaa187 <57514676+naaa187@users.noreply.github.com>
2023-01-04 18:59:47 +0900

    0105

---
+++ 0105/imp_18.py
@@ -0,0 +1,19 @@
+#baekjoon 22858
+n,k = map(int,input().split())
+sList = list(map(int,input().split()))
+dList_temp = list(map(int,input().split()))
+
+dList = []
+for i in range(n):
+    dList.append([dList_temp[i],i])
+
+dList.sort()
+
+ans = [sList[i] for i in range(n)]
+for i in range(k):
+    temp = []
+    for d in dList:
+        temp.append(ans[d[1]])
+    for j in range(n):
+        ans[j] = temp[j]
+print(*ans)
